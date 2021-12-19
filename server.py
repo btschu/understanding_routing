257680b473d690654d5aba908f08cf194f7aa119
Brandon Schumacher <92617960+btschu@users.noreply.github.com>
2021-12-18 22:18:57 -0700

    finished assignment

--- server.py
+++ server.py
@@ -1,8 +1,26 @@
-from flask import Flask  # Import Flask to allow us to create our app
+from flask import Flask
 
-app = Flask(__name__)    # Create a new instance of the Flask class called "app"
-@app.route('/')          # The "@" decorator associates this route with the function immediately following
+app = Flask(__name__)
+
+@app.route('/')
 def index():
-    return 'Python is running!'  # Return the string 'Hello World!' as a response
-if __name__=="__main__":   # Ensure this file is being run directly and not from a different module  
-    app.run(debug=True)    # Run the app in debug mode.
+    return 'Hello World!'
+
+@app.route('/dojo')
+def dojo():
+    return 'Dojo!'
+
+@app.route('/say/<string:name>')
+def say(name):
+    return f"Hi {name}"
+
+@app.route('/repeat/<int:num>/<string:name>')
+def repeat(num, name):
+    return f"{name * num}"
+
+@app.errorhandler(404)
+def pageNotFound(missing):
+    return "Sorry! No response. Try again."
+
+if __name__=="__main__":
+    app.run(debug=True)
